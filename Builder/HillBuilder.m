2a1f3d21cb89f8f48bf2fbfe0c5099e155cd0610
Andrew Pontious <apontious@gmail.com>
2017-11-27 08:00:23 -0800

    Further work on HillBuilder (not finished).

--- Builder/HillBuilder.m
+++ Builder/HillBuilder.m
@@ -6,15 +6,29 @@
 //  Copyright © 2017 Andrew Pontious. All rights reserved.
 //
 
+#import "CountUtils.h"
 #import "FileUtils.h"
 #import "HillBuilder.h"
 
 static NSString *const kHillAppFolderName = @"Hill";
 
-static NSString *const kHillAppMainFileName = @"main.m";
+static NSString *const kHillAppDelegateSourceName = @"AppDelegate.m";
+
+static NSString *const kHillAppDelegateSourceTemplate;
+static NSString *const kHillAppDelegateSourceImportReplacementsString = @"IMPORT_REPLACEMENTS";
+static NSString *const kHillAppDelegateSourceMethodCallReplacementsString = @"METHOD_CALL_REPLACEMENTS";
+
+static NSString *const kHillHeaderTemplate;
+static NSString *const kHillSourceTemplate;
+static NSString *const kHillBaseNumberReplacementString = @"BASE_NUMBER";
 
 static NSString *const kHillTestsFolderName = @"HillTests";
 static NSString *const kHillTestFileName = @"HillTests.m";
+
+static NSString *const kHillTestFileTemplate;
+static NSString *const kHillTestTemplate;
+static NSString *const kHillTestFileTestsReplacementString = @"TEST_REPLACEMENTS";
+
 @implementation HillBuilder
 
 - (instancetype)initWithCount:(NSUInteger)count antBuilder:(AntBuilder *)antBuilder {
@@ -27,22 +41,25 @@ static NSString *const kHillTestFileName = @"HillTests.m";
 }
 
 - (void)createHillWithProjectPath:(NSString *)projectPath {
+    FileUtils *fileUtils = [[FileUtils alloc] initWithProjectPath:projectPath];
+
     [self _hill_deleteExistingFilesWithProjectPath:projectPath];
 
-    [self _hill_createHeadersAndSourceWithProjectPath:projectPath];
-    [self _hill_createTestFileWithProjectPath:projectPath];
+    [self _hill_createAppDelegateSourceWithFileUtils:fileUtils];
+    [self _hill_createHeadersAndSourceWithFileUtils:fileUtils];
+    [self _hill_createTestFileWithFileUtils:fileUtils];
 }
 
 #pragma mark - Private Methods
 
 - (void)_hill_deleteExistingFilesWithProjectPath:(NSString *)projectPath {
 
-    FileUtils *fileUtils = [[FileUtils alloc] initWithProjectPath:kHillAppMainFileName];
+    FileUtils *fileUtils = [[FileUtils alloc] initWithProjectPath:projectPath];
 
     // Hill
-    //    main.m
+    //    AppDelegate.m
 
-    [fileUtils deleteFileInFolder:kHillAppFolderName withName:kHillAppMainFileName];
+    [fileUtils deleteFileInFolder:kHillAppFolderName withName:kHillAppDelegateSourceName];
 
     // Hill
     //    Hill00001.h/.m, etc.
@@ -57,10 +74,63 @@ static NSString *const kHillTestFileName = @"HillTests.m";
     [fileUtils deleteFileInFolder:kHillTestsFolderName withName:kHillTestFileName];
 }
 
-- (void)_hill_createHeadersAndSourceWithProjectPath:(NSString *)projectPath {
+- (void)_hill_createAppDelegateSourceWithFileUtils:(FileUtils *)fileUtils {
+    NSArray<NSString *> *baseNumberStrings = [CountUtils numberStringsForCount:self.count];
+
+    // Replacement #1: imports
+
+    NSMutableString *headerFileNamesString = [NSMutableString new];
+
+    for (NSString *baseNumberString in baseNumberStrings) {
+        [headerFileNamesString appendFormat:@"#import \"Hill%@.h\"\n", baseNumberString];
+    }
+
+    NSString *modifiedTemplate = [kHillAppDelegateSourceTemplate stringByReplacingOccurrencesOfString:kHillAppDelegateSourceImportReplacementsString
+                                                                                           withString:headerFileNamesString];
+
+    // Replacement #2: method calls
+
+    NSMutableString *methodCallsString = [NSMutableString new];
+
+    for (NSString *baseNumberString in baseNumberStrings) {
+        [methodCallsString appendFormat:@"    NSLog(@\"%%@\", [Hill%@ hill%@];\n", baseNumberString, baseNumberString];
+    }
+
+    [fileUtils writeFileWithTemplate:modifiedTemplate
+                     replacingString:kHillAppDelegateSourceMethodCallReplacementsString
+                          withString:methodCallsString
+                            inFolder:kHillAppFolderName
+                            withName:kHillAppDelegateSourceName];
 }
 
-- (void)_hill_createTestFileWithProjectPath:(NSString *)projectPath {
+- (void)_hill_createHeadersAndSourceWithFileUtils:(FileUtils *)fileUtils {
+}
+
+- (void)_hill_createTestFileWithFileUtils:(FileUtils *)fileUtils {
 }
 
 @end
+
+static NSString *const kHillAppDelegateSourceTemplate = \
+@"//\n\
+//  AppDelegate.m\n\
+//  Hill\n\
+//\n\
+//  Created by Andrew Pontious on 11/21/17.\n\
+//  Copyright © 2017 Andrew Pontious. All rights reserved.\n\
+//\n\
+\n\
+#import \"AppDelegate.h\"\n\
+\n\
+FRAMEWORK_IMPORT_REPLACEMENTS\n\
+@implementation AppDelegate\n\
+\n\
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {\n\
+\n\
+METHOD_CALL_REPLACEMENTS\n\
+\n\
+    return YES;\n\
+}\n\
+\n\
+@end\n\
+";
